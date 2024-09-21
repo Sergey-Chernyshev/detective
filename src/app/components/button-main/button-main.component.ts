8630c12c97a634736eea4ede4df154ef1c247204
Sergey-Chernyshev <gebodiksch@gmail.com>
2024-09-21 19:22:59 +0300

    mobile view

--- src/app/components/button-main/button-main.component.ts
+++ src/app/components/button-main/button-main.component.ts
@@ -1,4 +1,5 @@
-import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {Component, EventEmitter, inject, Input, Output} from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-button-main',
@@ -10,10 +11,17 @@ import {Component, EventEmitter, Input, Output} from '@angular/core';
 export class ButtonMainComponent {
 
   @Input() buttonText: string = ""
+  @Input() toTargetLink: string = ""
   @Output() buttonClick = new EventEmitter<void>();
+
+  readonly #router = inject(Router)
 
   onClick(): void {
     this.buttonClick.emit();
+
+    if(this.toTargetLink){
+      this.#router.navigate([''], {fragment: 'form'}).then(r => console.log(r));
+    }
   }
 
 }
